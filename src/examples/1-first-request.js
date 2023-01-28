d06f748ab9c004a0ae1670b25763f2ed9148dabd
tomix421q <zilka.tomas421@gmail.com>
2023-01-28 17:14:51 +0100

    axios library

--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,12 +1,25 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
+import axios from 'axios'
 // limit, if 429 wait for 15 min and try again
-const url = 'https://course-api.com/react-store-products';
+
+const url = 'https://course-api.com/react-store-products'
 
 const FirstRequest = () => {
-  useEffect(() => {
-    console.log('first axios request');
-  }, []);
+  const fetchData = async () => {
+    try {
+      const response = await axios(url)
 
-  return <h2 className='text-center'>first request</h2>;
-};
-export default FirstRequest;
+      const data = response.data
+      console.log(data)
+    } catch (error) {
+      console.log(error.response)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  return <h2 className='text-center'>first request</h2>
+}
+export default FirstRequest
